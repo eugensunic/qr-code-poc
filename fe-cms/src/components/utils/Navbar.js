9e012c3eafd898eaf771fa05fb231a6a9c032d00
Alan Kinkela Ramirez <akinkelar@outlook.com>
2019-12-11 12:59:59 +0100

    change navbar from grid to inline-block, responsive now

--- fe-cms/src/components/utils/Navbar.js
+++ fe-cms/src/components/utils/Navbar.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { isLoggedIn } from '../../helpers';
 
 function Navbar(route) {
+  const [toggleHeader, setToggleHeader] = useState(false);
   const logout = () => {
     fetch('/logout', {
       method: 'POST'
     }).then(_ => route.history.push('/'));
   };
+  // const[navclass,setNavclass] = "hero-nav";
 
   return (
-    <nav className="hero-nav">
+    <nav className={`hero-nav ${!toggleHeader && 'hidden-header'}`}>
+      <Link className="hamburger" onClick={() => setToggleHeader(!toggleHeader)}>
+        <i className="fa fa-bars"></i>
+      </Link>
       <ul className="hero-nav-ul">
         <li>
           <Link to={'/overview'}> Overview</Link>
         </li>
         <li>
-          <Link to={'/create'}> Create content</Link>
+          <Link to={'/create'}>New content</Link>
         </li>
         <li className="dropdownList">
           <Link to={'/changepassword'}>Admin</Link>
@@ -24,7 +29,7 @@ function Navbar(route) {
         <li>
           <Link to={'/about'}> About</Link>
         </li>
-        <li>
+        <li className="logoutLi">
           <button className="logoutButton" onClick={logout}>
             Logout
           </button>
